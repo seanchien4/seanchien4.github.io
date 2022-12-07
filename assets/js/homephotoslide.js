edc4105f64e7dec732e0011108e05600219c294e
seanchien4 <115194066+seanchien4@users.noreply.github.com>
2022-12-07 11:51:16 -0600

    test

--- assets/js/homephotoslide.js
+++ assets/js/homephotoslide.js
@@ -1,28 +1,39 @@
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+let slideIndex = 0;
+let timeoutId = null;
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
+
+showSlides();
+function currentSlide(index) {
+  slideIndex = index;
+  showSlides();
 }
+function plusSlides(step) {
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+  if (step < 0) {
+    slideIndex -= 2;
+
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+    }
+  }
+
+  showSlides();
 }
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove('active');
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1
   }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add('active');
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
